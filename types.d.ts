abdb7224d7426896bc1b4e191dfd1498f65f70b3
Tim Perry <pimterry@gmail.com>
2023-06-12 17:48:46 +0200

    Make the stream input type in our types more specific

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,3 @@
 export class XzReadableStream extends ReadableStream<Uint8Array> {
-    constructor(compressedStream: ReadableStream);
+    constructor(compressedStream: ReadableStream<Uint8Array>);
 }
